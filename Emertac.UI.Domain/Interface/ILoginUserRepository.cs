4d60307d76f43d83760d8eabcc42ca890da9dc59
Sandip KumbHani <Sandip@emertec.co.uk>
2025-07-14 12:10:48 +0530

    Delete UI project.

--- Emertac.UI.Domain/Interface/ILoginUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Emertac.UI.Domain.DTO;
-namespace Emertac.UI.Domain.Interface
-{
-    public interface ILoginUserRepository
-    {
-        Task<bool> LoginUserAsync(LoginUserDTO loginUserDTO);
-    }
-}
